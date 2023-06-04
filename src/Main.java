0565f665a9485472ac19e7b3366c551b206ea97a
matias landau <landaumatias@gmail.com>
2023-06-04 19:09:11 -0300

    main test
    Signed-off-by: matias landau <landaumatias@gmail.com>

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import uade.edu.ar.backtraking.batalla.Resultado;
+import uade.edu.ar.backtraking.batalla.Ubicacion;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Resultado a = new Resultado(new ArrayList<Ubicacion>());
+    }
+
+}
